190f5b1844a4620fc680679f57623f48ea78d26a
BOK-MINJEONG <129048011+BOK-MINJEONG@users.noreply.github.com>
2023-11-09 22:31:32 +0900

    #1-manager 추가: manager 헤더, cpp 파일 추가

---
+++ Color.univ/manager.cpp
@@ -0,0 +1,38 @@
+#include "manager.h"
+#include "util.h"
+
+//void setScore();
+//
+//void drawStartScreen();
+//
+//void drawMenu();
+//
+void drawDevInfo() { // 임시화면
+	setCurrentCursorPos(10, 10);
+	std::cout << "[Developer Info]" << endl;
+
+	setCurrentCursorPos(15, 11);
+	std::cout << "복민정" << endl;
+
+	setCurrentCursorPos(15, 13);
+	std::cout << "이지호" << endl;
+
+	setCurrentCursorPos(15, 14);
+	std::cout << "신지우" << endl;
+
+	setCurrentCursorPos(15, 15);
+	std::cout << "전지원" << endl;
+
+}
+//
+//void drawStageIntroInfo();
+//
+//void drawStageEnding();
+//
+//void drawGameOver();
+//
+//void drawGameInstruction();
+//
+//void drawIntro();
+//
+//void drawReturnToMain();
---
+++ Color.univ/manager.h
@@ -0,0 +1,28 @@
+#ifndef __MANAGER_H__
+#define __MANAGER_H_
+
+#include <iostream>
+using namespace std;
+
+//void setScore();
+//
+//void drawStartScreen();
+//
+//void drawMenu();
+//
+void drawDevInfo();
+//
+//void drawStageIntroInfo();
+//
+//void drawStageEnding();
+//
+//void drawGameOver();
+//
+//void drawGameInstruction();
+//
+//void drawIntro();
+//
+//void drawReturnToMain();
+
+
+#endif
