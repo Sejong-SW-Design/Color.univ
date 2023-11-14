8098067daacf2d1944ba0ad89a09b45a413fb32d
Jun Jiwon <ysjjw2003@naver.com>
2023-11-15 02:29:25 +0900

    [update] 벽 완전히 해제되도록 2학년 맵 살짝 변경

--- Color.univ/main.cpp
+++ Color.univ/main.cpp
@@ -14,11 +14,11 @@ int main() {
 	setConsoleSize();
 	removeCursor();
 
-	drawGameBoard(gameMap1); //gameMap1
+	drawGameBoard(gameMap2); //gameMap1
 
     //초기위치 정해주세요! (게임보드 기준! 곱하기 2 이딴거 안해도됨)
-    Pos playerInitPos = { 1,10 }; //1,10 - 1  // 16,1 - 2 
-    Pos enemyInitPos = { 26, 5 }; //26,5 - 1  // 30,5 - 2
+    Pos playerInitPos = { 16,1}; //1,10 - 1  // 16,1 - 2 
+    Pos enemyInitPos = { 30, 5 }; //26,5 - 1  // 30,5 - 2
     player = new Player(playerInitPos);
     enemy = new Enemy(enemyInitPos, 300);
 
@@ -26,7 +26,7 @@ int main() {
     {
         for (int i = 0; i < 50; i++)
         {
-            player->moveingProcess(gameMap1);
+            player->moveingProcess(gameMap2);
             if (player->checkGoalIn())
             {
                 drawResultScreen(gameOver, 0);
