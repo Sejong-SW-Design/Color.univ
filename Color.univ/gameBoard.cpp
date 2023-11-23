63e0104ca357e0c88dddc96f0bdbad9dcf93ad0d
Jun Jiwon <ysjjw2003@naver.com>
2023-11-23 22:47:50 +0900

    4학년 맵 npc제외 완성

--- Color.univ/gameBoard.cpp
+++ Color.univ/gameBoard.cpp
@@ -178,7 +178,7 @@ void drawOnePoint(int gameMap[22][37], int i, int j, int backGround)
     case LIFE:
         setBackgroundColor(backGround, 4); printf("¢¾"); break; // npc¶û Çò°¥¸±±îºÁ ´ÙÅ© ·¹µå »ç¿ë
     case SPEED:
-        setBackgroundColor(backGround, 3); printf("¨ß"); break; // eraser¶û Çò°¥¸±±îºÁ ¾îµÎ¿î ÇÏ´Ã »ç¿ë
+        setBackgroundColor(backGround, 7); printf("©ß"); break; // eraser¶û Çò°¥¸±±îºÁ
     case NORMAL_NPC:
         setBackgroundColor(backGround, 12); printf("¡â"); break;
     case ALCOHOL_NPC:
